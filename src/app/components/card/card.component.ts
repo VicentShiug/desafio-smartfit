6c99d50773fb841305daa8122a6c8a0e5219494f
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-10-19 10:10:13 -0300

    feat: create unit card list

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from 'src/app/types/location.interface';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() card!: Location;
+  
+  ngOnInit(): void {
+    
+  }
+}
