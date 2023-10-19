4cd712fb78e30a44e40d464323be856fd4dfc5c0
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-10-19 10:07:40 -0300

    feat: moved filter and card list to get unit service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { FitlerUnitsService } from 'src/app/services/fitler-units.service';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 import { Location } from 'src/app/types/location.interface';
 
@@ -12,7 +12,7 @@ import { Location } from 'src/app/types/location.interface';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-
+  @Output() submitEvent = new EventEmitter();
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
@@ -20,7 +20,7 @@ export class FormsComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private unitService: GetUnitsService,
-    private filterUnitsService: FitlerUnitsService
+    private filterUnitsService: FilterUnitsService
   ) { }
 
   ngOnInit(): void {
@@ -28,16 +28,19 @@ export class FormsComponent implements OnInit {
       hour: '',
       showClosed: true,
     })
-    this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations
-      this.filteredResults = data.locations
+    this.unitService.getAllUnits().subscribe(data => { 
+      this.results = data
+      this.filteredResults = data
     })
   }
 
   onSubmit(): void {
-    const { value } = this.formGroup
-    const { hour, showClosed } = value
-    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour)
+    const { value } = this.formGroup;
+    const { hour, showClosed } = value;
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
   }
 
   onClean(): void {
